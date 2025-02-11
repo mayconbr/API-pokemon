1ebbd90e9d44eb5fa9997412930bc0f9fb720ce6
Maycon Bruzolato <65423721+mayconbr@users.noreply.github.com>
2025-02-11 09:47:07 -0300

    firt migration

---
+++ Migrations/20250211124520_create-db.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pokedex.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "latin1");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    email = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    password = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    hash = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    typeUser = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Treiners",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    userId = table.Column<long>(type: "bigint", nullable: false),
+                    name = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    region = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    initialPokemon = table.Column<string>(type: "longtext", nullable: true, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    creationDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    dateDelete = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treiners", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Treiners_Users_userId",
+                        column: x => x.userId,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    trainerId = table.Column<long>(type: "bigint", nullable: false),
+                    name = table.Column<string>(type: "longtext", nullable: true, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    pokemon = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    type = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1"),
+                    pokedexNumber = table.Column<string>(type: "longtext", nullable: false, collation: "latin1_swedish_ci")
+                        .Annotation("MySql:CharSet", "latin1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Pokemons_Treiners_trainerId",
+                        column: x => x.trainerId,
+                        principalTable: "Treiners",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "latin1")
+                .Annotation("Relational:Collation", "latin1_swedish_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokemons_trainerId",
+                table: "Pokemons",
+                column: "trainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treiners_userId",
+                table: "Treiners",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+
+            migrationBuilder.DropTable(
+                name: "Treiners");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
